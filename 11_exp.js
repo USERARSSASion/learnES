ad135c0076c588fca7949c82323790adefb18ff5
锦龙 <majinlong@uniwyse.com>
2024-04-18 14:27:32 +0800

    add

--- 11_exp.js
+++ 11_exp.js
@@ -2,7 +2,7 @@
  * @Author: majl
  * @Date: 2023-10-12 16:13:58
  * @LastEditors: majl
- * @LastEditTime: 2024-04-18 11:58:27
+ * @LastEditTime: 2024-04-18 14:27:28
  * @FilePath: /learnES/11_exp.js
  * @Description: 
  * 
@@ -109,6 +109,151 @@
                 				}
         					}
         				}
+					}
+				}
+			}
+		}
+	}
+}
+
+# 反向嵌套聚合
+{
+	"track_total_hits": true,
+	"query": {
+		"bool": {
+			"must": [
+				{
+					"range": {
+						"stats_day": {
+							"from": "2024/03/01 07:00:00 +08:00",
+							"to": "2024/03/01 08:00:00 +08:00"
+						}
+					}
+				},
+				{
+					"term": {
+						"_id": "JP_AMZ_ECOFLOW_B0C378GM3C_ROLH94SBC4AAP_20240312"
+					}
+				},
+				{
+					"nested": {
+						"path": "product_infos",
+						"query": {
+							"bool": {
+								"must": [
+									{
+										"terms": {
+											"product_infos.energy_capacity_level": [
+												"1000-1999wh"
+											]
+										}
+									}
+								]
+							}
+						}
+					}
+				}
+			],
+			"must_not": [],
+			"should": []
+		}
+	},
+	"from": 0,
+	"size": 100,
+	"aggs": {
+		"product_infos_1": {
+			"nested": {
+				"path": "product_infos"
+			},
+			"aggs": {
+				"type": {
+					"terms": {
+						"field": "product_infos.type_code",
+						"include": "product_main"
+					},
+					"aggs": {
+						"level": {
+							"reverse_nested": {},
+							"aggs": {
+								"trends": {
+									"date_histogram": {
+										"interval": "month",
+										"field": "stats_day",
+										"time_zone": "+08:00"
+									},
+									"aggs": {
+										"tendency": {
+											"terms": {
+												"field": "tendency"
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		"product_infos": {
+			"nested": {
+				"path": "product_infos"
+			},
+			"aggs": {
+				"type": {
+					"terms": {
+						"field": "product_infos.type_code",
+						"include": "product_main"
+					},
+					"aggs": {
+						"level": {
+							"terms": {
+								"field": "product_infos.energy_capacity_level"
+							},
+							"aggs": {
+								"trends": {
+									"date_histogram": {
+										"interval": "month",
+										"field": "stats_day",
+										"time_zone": "+08:00"
+									},
+									"aggs": {
+										"tendency": {
+											"terms": {
+												"field": "tendency"
+											},
+											"aggs": {
+												"reverse": {
+													"reverse_nested": {}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		"type": {
+			"terms": {
+				"field": "products.energy_capacity_level",
+				"size": 10
+			},
+			"aggs": {
+				"trends": {
+					"date_histogram": {
+						"interval": "month",
+						"field": "stats_day",
+						"time_zone": "+08:00"
+					},
+					"aggs": {
+						"tendency": {
+							"terms": {
+								"field": "tendency"
+							}
+						}
 					}
 				}
 			}
